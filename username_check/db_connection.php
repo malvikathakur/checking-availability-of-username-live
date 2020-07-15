295ee24d8100aba86907eaa7c031e8a35e065b41
malvikathakur <malvikathakur@users.noreply.github.com>
2020-07-15 19:32:44 +0530

    updating files

---
+++ username_check/db_connection.php
@@ -0,0 +1,14 @@
+<?php
+// Connection variables
+$host = "localhost";
+
+$user = "root";
+$password = "";
+$database = "1"; // MySQL Database name
+// Connect to MySQL Database
+$con = new mysqli($host, $user, $password, $database);
+// Check connection
+if ($con->connect_error) {
+die("Connection failed: " . $con->connect_error);
+}
+?>
